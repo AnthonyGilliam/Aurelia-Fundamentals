1468be207c2cc0b54faee597b84160f71c89f231
Anthony <anthony.gilliam@gmail.com>
2017-08-25 14:42:35 -0400

    Data Binding Beyond the Basics - Specialized Input Bindings: Select, Radio, and Checkbox #1

--- CommunityApp/src/viewmodels/events/eventsList.js
+++ CommunityApp/src/viewmodels/events/eventsList.js
@@ -7,6 +7,7 @@ export class EventsList {
 	constructor(repo, router) {
 		this.repo = repo;
 		this.router = router;
+		this.whoAreYou = "Luke, I am your father.";
 	}
 
     goToDiscussion(){
--- CommunityApp/src/viewmodels/sideBar/sponsors.js
+++ CommunityApp/src/viewmodels/sideBar/sponsors.js
@@ -1,8 +1,17 @@
 export class Sponsors {
 	constructor() {
-		this.message = "Sponsors";
-		setTimeout(() => this.message = "Changed after binding", 3000);
-	}
+        this.message = "Sponsors";
+        setTimeout(() => this.message = "Changed after binding", 3000);
+        this.mapCollection = new window.Map();
+        this.mapCollection.set("a", "Alpha");
+        this.mapCollection.set("b", "Beta");
+        this.mapCollection.set("c", "Charlie");
+        this.mapCollection.set("d", "Delta");
+        this.styleString = "background: red";
+        this.styleObject = { background: "green" };
+        this.customerColor = 'purple';
+        this.customerStatus = 'bad';
+    }
 
 	doSomething(message) {
 		console.log(message);
