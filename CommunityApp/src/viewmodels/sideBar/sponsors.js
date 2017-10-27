8969b09bcc3b3fa18c9bd3cc46d470b810776915
Anthony <anthony.gilliam@gmail.com>
2017-10-27 17:55:53 -0400

    Data Binding Beyond the Basics - Avoiding Dirty Checking

--- CommunityApp/src/viewmodels/sideBar/sponsors.js
+++ CommunityApp/src/viewmodels/sideBar/sponsors.js
@@ -1,3 +1,4 @@
+import {computedFrom} from 'aurelia-framework';
 export class Sponsors {
 	constructor() {
         this.message = "Sponsors";
@@ -11,9 +12,17 @@ export class Sponsors {
         this.styleObject = { background: "green" };
         this.customerColor = 'purple';
         this.customerStatus = 'bad';
+        this.person = new Person();
     }
 
 	doSomething(message) {
 		console.log(message);
 	}
+}
+
+class Person {
+    firstName = "Brian";
+    lastName = "Noyes";
+    @computedFrom("firstName", "lastName")
+    get fullName() { return this.firstName + " " + this.lastName; }
 }
